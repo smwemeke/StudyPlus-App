8c59c85b38a440763f51e8f38fa6e4bbdf5bde22
smwemeke <sue.mwemeke@gmail.com>
2024-11-30 22:28:15 -0700

    added permissions

--- studyPlus/src/main/java/edu/miu/cs489/studyplus/model/Permission.java
+++ studyPlus/src/main/java/edu/miu/cs489/studyplus/model/Permission.java
@@ -1,4 +1,16 @@
 package edu.miu.cs489.studyplus.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum Permission {
+    COORDINATOR_READ("coord:read"),
+    COORDINATOR_WRITE("coord:write"),
+
+    PARTICIPANT_READ("part:read"),
+    PARTICIPANT_WRITE("part:write");
+
+    private final String permission;
 }
--- studyPlus/src/main/java/edu/miu/cs489/studyplus/model/Role.java
+++ studyPlus/src/main/java/edu/miu/cs489/studyplus/model/Role.java
@@ -1,6 +1,22 @@
 package edu.miu.cs489.studyplus.model;
 
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+@RequiredArgsConstructor
 public enum Role {
-    COORDINATOR,
-    PARTICIPANT
+    COORDINATOR(
+            Set.of(Permission.COORDINATOR_WRITE,
+                    Permission.COORDINATOR_READ)
+    ),
+    PARTICIPANT(
+            Set.of(
+                    Permission.PARTICIPANT_WRITE,
+                    Permission.PARTICIPANT_READ
+            )
+    );
+    private final Set<Permission> permissions;
+
+
 }
